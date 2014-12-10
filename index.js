a71c0b5f7eaba028fee4fb196094f7b331a0e507
Ross Kukulinski <ross@kukulinski.com>
2014-12-09 16:16:28 -0800

    Initial commit of coding challenege

---
+++ index.js
@@ -0,0 +1,43 @@
+var debug = require('debug')('frontend-code-challenge');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('./lib/logger');
+
+var users = require('./routes/users');
+
+var app = express();
+var log = logger(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+app.use(function(err, req, res, next) {
+  log.error(err);
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  log.info('Express server listening on http://localhost:%d', server.address().port);
+});
---
+++ routes/users.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+var logger = require('../lib/logger');
+var log = logger();
+
+var users = require('../init_data.json').data;
+var curId = _.size(users);
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.json(_.toArray(users));
+});
+
+/* Create a new user */
+router.post('/', function(req, res) {
+  var user = req.body;
+  user.id = curId++;
+  if (!user.status) {
+    user.status = 'pending';
+  }
+  users[user.id] = user;
+  log.info('Created user', user);
+  res.json(user);
+});
+
+/* Get a specific user by id */
+router.get('/:id', function(req, res, next) {
+  var user = users[req.params.id];
+  if (!user) {
+    return next();
+  }
+  res.json(users[req.params.id]);
+});
+
+/* Delete a user by id */
+router.delete('/:id', function(req, res) {
+  var user = users[req.params.id];
+  delete users[req.params.id];
+  res.status(204);
+  log.info('Deleted user', user);
+  res.json(user);
+});
+
+/* Update a user by id */
+router.put('/:id', function(req, res) {
+  var user = req.body;
+  if (user.id != req.params.id) {
+    return next();
+  }
+  users[user.id] = user;
+  log.info('Updating user', user);
+  res.json(user);
+});
+
+
+module.exports = router;
